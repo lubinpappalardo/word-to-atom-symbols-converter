63df2256446985dfeecb7f719b7e47a1a8d2d467
Lubin Pappalardo <lubinpappalardo@gmail.com>
2023-11-15 20:08:56 -0500

    Css for the elements

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ var elementsSymbols = [
     "Na", "Mg",                                                             "Al", "Si", "P", "S", "Cl", "Ar", 
     "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", 
     "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", 
-    "Cs", "Ba",      "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
-    "Fr", "Ra",     "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og",
-                    "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", 
-                    "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr" 
+    "Cs", "Ba",     "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu",    "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
+    "Fr", "Ra",     "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr" ,   "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"
  ];
  
  // list of full names of the elements
@@ -18,10 +16,8 @@ var elementsFullNames = [
     "Sodium", "Magnesium",                                                  "Aluminum", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon", 
     "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", 
     "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", 
-    "Cesium", "Barium",             "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon",
-    "Francium", "Radium",           "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tennessine", "Oganesson",
-                                    "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium",
-                                    "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", "Lawrencium"
+    "Cesium", "Barium",             "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium",                "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon",
+    "Francium", "Radium",           "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", "Lawrencium",        "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tennessine", "Oganesson"
  ];
 
 //  console.log(elementsSymbols.length);
@@ -79,9 +75,9 @@ var elementsFullNames = [
                    isLastTry = false;
                    translate(1);
                 } else {
-                    document.getElementById("result").innerHTML = "Translated word: " + result.join(" ") + "<br>";
+                    document.getElementById("result").innerHTML = "";
                     for (var i = 0; i < result.length; i++) {
-                        document.getElementById("result").innerHTML += elementsFullNames[elementsSymbols.indexOf(result[i])] + "<br>";
+                        document.getElementById("result").innerHTML += `<div class='element'><span class='element-number'>${elementsSymbols.indexOf(result[i]) + 1}</span>${result[i]}<span class='element-name'>${elementsFullNames[elementsSymbols.indexOf(result[i])]}</span></div>`;
                     }
                     return;
                 }
@@ -108,11 +104,11 @@ var elementsFullNames = [
                    lastMove = 1;
                    translate(1);
                 } else {
-                    document.getElementById("result").innerHTML = "Translated word: " + result.join(" ") + "<br>";
+                    document.getElementById("result").innerHTML = "";
                     for (var i = 0; i < result.length; i++) {
-                       document.getElementById("result").innerHTML += elementsFullNames[elementsSymbols.indexOf(result[i])] + "<br>";
+                        document.getElementById("result").innerHTML += `<div class='element'><span class='element-number'>${elementsSymbols.indexOf(result[i]) + 1}</span>${result[i]}<span class='element-name'>${elementsFullNames[elementsSymbols.indexOf(result[i])]}</span></div>`;
                     }
-                   return;
+                    return;
                 }
             } else if (isLastTry) {
                 document.getElementById("result").innerHTML = "This word can't be translated: " + result.join(" ");
